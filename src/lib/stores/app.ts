daf2daa7b39c579ed23e6d705ed74467abc5a706
696y-source <ckdt8c696y@privaterelay.appleid.com>
2026-06-02 21:10:34 +0600

    Update app.ts

--- src/lib/stores/app.ts
+++ src/lib/stores/app.ts
@@ -14,7 +14,7 @@ const storeInit: {
   locale: string,
   translations: Record<TranslationKeys, string> | {}
 } = {
-  api: import.meta.env.DEV ? '' : 'https://api.ytify.workers.dev',
+  api: '',
   useSaavn: true,
   locale: initLocale,
   translations: {},
@@ -22,21 +22,23 @@ const storeInit: {
 
 export const [store, setStore] = createStore(storeInit);
 
-
 export function t(key: TranslationKeys, value: string = ''): string {
 
   const translations = store.translations as Record<TranslationKeys, string>;
   const translatedString = translations[key] || key as string;
-  return value ? translatedString.replace('$', value) : translatedString;
+
+  return value
+    ? translatedString.replace('$', value)
+    : translatedString;
 }
 
 export async function updateLang() {
 
   document.documentElement.lang = store.locale;
 
-  const json = await import(`../../locales/${store.locale}.json`)
+  const json = await import(`../../locales/${store.locale}.json`);
 
   setStore('translations', json.default);
-  return true;
 
+  return true;
 }
