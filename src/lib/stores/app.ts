f421639dc7e681e3f33cd2da192dc4ab6a3590a7
696y-source <ckdt8c696y@privaterelay.appleid.com>
2026-06-03 12:19:04 +0600

    Update app.ts

--- src/lib/stores/app.ts
+++ src/lib/stores/app.ts
@@ -1,8 +1,11 @@
 import { config } from "../utils/config";
 import { createStore } from "solid-js/store";
+
 const nl = navigator.language.slice(0, 2);
+
 const initLocale =
   config.language || (Locales.includes(nl) ? nl : 'en');
+
 const storeInit: {
   useSaavn: boolean,
   api: string,
@@ -13,29 +16,39 @@ const storeInit: {
   locale: string,
   translations: Record<TranslationKeys, string> | {}
 } = {
-  api: 'https://wtube-api.tahsin-hassan-2627.workers.dev',
+  api: '',
   useSaavn: true,
   locale: initLocale,
   translations: {},
 };
+
 export const [store, setStore] = createStore(storeInit);
+
 export function t(
   key: TranslationKeys,
   value: string = ''
 ): string {
+
   const translations =
     store.translations as Record<TranslationKeys, string>;
+
   const translatedString =
     translations[key] || key as string;
+
   return value
     ? translatedString.replace('$', value)
     : translatedString;
 }
+
 export async function updateLang() {
+
   document.documentElement.lang = store.locale;
+
   const json = await import(
     `../../locales/${store.locale}.json`
   );
+
   setStore('translations', json.default);
+
   return true;
 }
